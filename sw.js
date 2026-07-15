83ab1668a6163c9534ca0f133d7875f5338e4308
aluiskun-boop <aluiskun@gmail.com>
2026-07-15 13:24:32 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='hell-corridor-v5-1';
+const CORE=['./hell_corridor_iv.html','./manifest.webmanifest','./app-icon.svg','./demon_sprites.png','./environment_atlas.png','./equipment_sprites.png','./effects_pickups_atlas.png','./infernal_reign_theme.mp3'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(response=>{let copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./hell_corridor_iv.html',copy));return response}).catch(()=>caches.match('./hell_corridor_iv.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok){let copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response})));
+});
