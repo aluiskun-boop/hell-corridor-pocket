7b22b1116bf50b590296c1ec914b10bd88bff3ad
aluiskun-boop <aluiskun@gmail.com>
2026-07-15 21:34:20 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hell-corridor-v5-5';
-const CORE=['./hell_corridor_iv.html','./manifest.webmanifest','./app-icon.svg','./demon_sprites.png','./environment_atlas.png','./equipment_sprites.png','./effects_pickups_atlas.png','./chainsaw_sprite.png','./stalker_sprites.png','./infernal_reign_theme.mp3','./stalker_taunt1.m4a','./stalker_taunt2.m4a','./stalker_hidden.m4a','./stalker_attack.m4a','./stalker_hurt.m4a','./stalker_death.m4a'];
+const CACHE='hell-corridor-v5-8';
+const CORE=['./hell_corridor_iv.html','./manifest.webmanifest','./app-icon.svg','./demon_sprites.png','./environment_atlas.png','./equipment_sprites.png','./effects_pickups_atlas.png','./chainsaw_sprite.png','./stalker_sprites.png','./goyboy_faces.png','./hell_scenery.png','./infernal_reign_theme.mp3','./stalker_taunt1.m4a','./stalker_taunt2.m4a','./stalker_hidden.m4a','./stalker_attack.m4a','./stalker_hurt.m4a','./stalker_death.m4a'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
