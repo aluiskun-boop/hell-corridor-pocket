9f1d4b590b8186b88b1b47a7f05ead219de5cdd9
aluiskun-boop <aluiskun@gmail.com>
2026-07-16 10:18:14 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hell-corridor-v6-5';
+const CACHE='hell-corridor-v6-6';
 const CORE=['./hell_corridor_iv.html','./manifest.webmanifest','./app-icon.svg','./demon_sprites.png','./environment_atlas.png','./equipment_sprites.png','./effects_pickups_atlas.png','./chainsaw_sprite.png','./stalker_sprites.png','./goyboy_faces.png','./hell_scenery.png','./infernal_props_atlas.png','./smolder_bomb.png','./vanover_smoking.png','./infernal_reign_theme.mp3','./demon_growl.mp3','./demon_attack.mp3','./demon_hurt.mp3','./demon_death.mp3','./boss_roar.mp3','./shooter_cast.mp3','./stalker_taunt1.m4a','./stalker_taunt2.m4a','./stalker_hidden.m4a','./stalker_attack.m4a','./stalker_hurt.m4a','./stalker_death.m4a'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
